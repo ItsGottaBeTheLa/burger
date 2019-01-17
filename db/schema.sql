597004b54651e04241e68e371b58b1af736c9851
Damiano Raab <DamianoRaab11@gmail.com>
2019-01-16 19:29:44 -0500

    completed config

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,11 @@
 create database burgers_db;
 
-use burger_db;
+use burgers_db;
 
 create table burgers
 (
 	id int not null auto_increment,
     burger_name varchar(200) not null,
-    devoured boolean deafult false,
+    devoured boolean default false,
     primary key (id)
 );
